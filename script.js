d2f35a27ff3f60d5d4cd4deb37a9f6865644e378
LegendOfDark <67946587+LegendOfDark@users.noreply.github.com>
2021-10-02 18:02:33 -0400

    The quiz now retains the variables in local storage now

--- script.js
+++ script.js
@@ -26,54 +26,99 @@ const questions = [
     }
 ]
 
-let question_num = 0;
-let correct = 0;
+// create local storage var
+if(!localStorage.getItem('question_num')){
+    localStorage.setItem('question_num', 0)
+    localStorage.setItem('correct', 0)
+} 
 
+// document loaded then ....
 document.addEventListener('DOMContentLoaded', () => {
     load_question()
 })
 
+// define vars
+let question_num;
+let correct;
 
 
-
+// run this function
 const load_question = () => {
+    
+    // get values from local storage
+    question_num = localStorage.getItem('question_num')
+    correct = localStorage.getItem('correct')
 
+    // show the current score
+    document.querySelector("#correct").innerHTML = `${correct} of ${question_num}` 
+
+    // get question div
     let questions_div = document.querySelector('#questions')
 
-    if (questions.length === question_num) {
+    // if quiz over
+    if (questions.length === parseInt(question_num)) {
+
+        // alert('hi')
+
+        //display msg & hide buttons
         questions_div.innerHTML = "Quiz Completed!"
         document.querySelectorAll(".option").forEach(opt => {
             opt.style.display = "none";
         })
+
+        // set local storage back to 0
+        localStorage.setItem('question_num', 0)
+        localStorage.setItem('correct', 0)
+        
         setTimeout(() => {
             location.reload();
-        }, 5000);
-        return;
+            return
+        }, 2000);
     }
+    
+    // if quiz not done ....
 
+    // insert the question in HTML
     questions_div.innerHTML = questions[question_num].question
+
+    // get the options div and empty it
     const options = document.querySelector('#options')
     options.innerHTML = ""
 
+    // insert buttons with value (answers)
     questions[question_num].options.forEach (opt => {
         options.innerHTML += `<button class="option">${opt}</button>`
     })
 
+    // if button clicked ...
     document.querySelectorAll(".option").forEach(opt => {
         opt.onclick = () => {
+
+            // check if right answer
             if (opt.textContent === questions[question_num].answer)
             {
+                // Update correct value and Local storage
                 opt.style.background = 'green';
                 correct++
+                localStorage.setItem('correct', correct)   
             }
             else{
                 opt.style.background = 'red';
             }
+
+            // update question num and storage
             question_num++
+            localStorage.setItem('question_num', question_num)
+            // alert('question_num is ' + question_num)
+            // alert('correct is ' + correct)
+
+            // grab correct div and update value
             document.querySelector("#correct").innerHTML = `${correct} of ${question_num}` 
+            
+            // load the function again
             setTimeout(() => {
                 load_question()
-            }, 1000);
+            }, 750);
         }
     })
 
